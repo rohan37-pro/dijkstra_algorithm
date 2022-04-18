d9bfbd5b00ef673527b04459ff34cdf3a26061ac
rohan37-pro <rohan2002maji@gmail.com>
2022-04-18 03:46:22 -0400

    unwanted lines are deleted

--- dijkstra_algo.py
+++ dijkstra_algo.py
@@ -14,7 +14,6 @@ demo_net = [[0, 4, 0, 0, 0, 0, 0, 8, 0],
 
 
 
-
 def add_node(net_array,g):
     node = 0
     total_path = 0
@@ -118,7 +117,6 @@ def dijkstra_algo(graph,source):
 
 
 
-
 if __name__ == "__main__":
     graph = eval(input("enter the array of the graph : "))
     source = int(input("enter the source node : "))
@@ -129,6 +127,3 @@ if __name__ == "__main__":
     savefile(graph,"graph of network.png")
     savefile(shorted_graph,"graph of shortest path.png")
  
-
-
-[[0, 4, 0, 0, 0, 0, 0, 8, 0],[4, 0, 8, 0, 0, 0, 0, 11, 0],[0, 8, 0, 7, 0, 4, 0, 0, 2],[0, 0, 7, 0, 9, 14, 0, 0, 0],[0, 0, 0, 9, 0, 10, 0, 0, 0],[0, 0, 4, 14, 10, 0, 2, 0, 0],[0, 0, 0, 0, 0, 2, 0, 1, 6],[8, 11, 0, 0, 0, 0, 1, 0, 7],[0, 0, 2, 0, 0, 0, 6, 7, 0]]
